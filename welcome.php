592512a6a596f711e12dea903d4a9bc81de00014
rania <118706583+raniasalsabila@users.noreply.github.com>
2024-01-15 13:30:57 +0700

    welcome.php

--- welcome.php
+++ welcome.php
@@ -6,27 +6,27 @@
     <title>formulir pendaftaran</title>
 </head>
 <body>
-    <h1>SELAMAT DATANG!</h1>
+<h1>SELAMAT DATANG!</h1>
     <h3>Anda telah bergabung sebagai:</h3>
-    <?php
+    <?php 
         $firstName = $_POST["firstName"];
         $lastName = $_POST["lastName"];
         $namaLengkap = $firstName . " " . $lastName;
-        echo "<p>Nama Lengkap : $namaLengkap</p>";
+        echo "<p>Nama Lengkap : $namaLengkap</p>";        
     ?>
-    <?php
+    <?php 
         $gender = $_POST["gender"];
-        echo "<p>Jenis Kelamin : $gender</p>";
+        echo "<p>Jenis Kelamin: $gender</p>";
     ?>
-    <?php
+    <?php 
         $nationality = $_POST["nationality"];
-        echo "<p>Kewarganegaraan : $nationality</p>";
+        echo "<p>Kewarngaraan: $nationality</p>";
     ?>
-    <?php
+    <?php 
         $bahasa = $_POST['languange'];
-        echo "<p>Bahasa yang dikuasai:" . implode(", ", $bahasa) . "</p>";
+        echo "<p>Bahasa yang dikuasai:". implode(", ", $bahasa) . "</p>";
     ?>
-    <?php
+    <?php 
         $bio = $_POST["bio"];
         echo "<p>Bio : $bio<p/>";
     ?>
